a69a15cebd8bacb7a11e3ef5f66a97afc3c08f7c
jmjuanes <josemijuanes@gmail.com>
2016-05-02 11:15:52 +0200

    Simplify the script

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
 //Get the CLI arguments
-module.exports = function(opt)
+module.exports = function()
 {
-	//Check for string
-	if(typeof opt === 'string'){ opt = { args: opt }; }
-	
-	//Check for undefined options
-	if(typeof opt === 'undefined'){ var opt = {}; }
+	//Get the node args
+	var args = process.argv;
 
-	//Check for custom arguments
-	if(typeof opt.args === 'undefined'){ opt.args = process.argv; }
+	//Check if the second command has a .js file
+	if(args[1].indexOf('.js') !== -1){ args.splice(1, 1); }
 
-	//Check for string args
-	if(typeof opt.args === 'string'){ opt.args = opt.args.split(' '); }
-
-	//Check for node command
-	if(typeof opt.node !== 'undefined' && opt.node === true)
-	{
-		//Check if the second command has a .js file
-		if(opt.args[1].indexOf('.js') !== -1){ opt.args.splice(1, 1); }
-
-		//Check if the first command is node
-		if(opt.args[0] === 'node' || opt.args[0] === 'nodejs'){ opt.args.splice(0, 1); }
-	}
+	//Check if the first command is node
+	if(args[0] === 'node' || args[0] === 'nodejs'){ args.splice(0, 1); }
 
 	//Output arguments
 	var out = { command: '', arguments: [], options: {} };
 
 	//Read all the arguments
-	for(var i = 0; i < opt.args.length; i++)
+	for(var i = 0; i < args.length; i++)
 	{
 		//Get the argument
-		var a = opt.args[i];
+		var a = args[i];
 
 		//Check if argument starts with -
 		if(a.substring(0, 1) !== '-')
@@ -58,15 +45,15 @@ module.exports = function(opt)
 		if(a.substring(0, 1) === '-'){ a = a.replace('-', ''); }
 
 		//Check for invalid argument
-		if(a === '' || a === ' '){ throw new Error('Invalid argument ' + opt.args[i]); }
+		if(a === '' || a === ' '){ throw new Error('Invalid argument ' + args[i]); }
 
 		//Check the next argument
-		if(i + 1 >= opt.args.length)
+		if(i + 1 >= args.length)
 		{
 			//Set the argument as true
 			out.options[a] = true;
 		}
-		else if(opt.args[i + 1].substring(0, 1) === '-')
+		else if(args[i + 1].substring(0, 1) === '-')
 		{
 			//Set the argument as true
 			out.options[a] = true;
@@ -74,7 +61,7 @@ module.exports = function(opt)
 		else
 		{
 			//Set the argument as value
-			out.options[a] = opt.args[i + 1];
+			out.options[a] = args[i + 1];
 
 			//Increment the i
 			i = i + 1;
