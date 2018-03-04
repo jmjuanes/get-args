961c1b063b257aa9c4cbf92512b1344485fcfb50
jmjuanes <josemijuanes@gmail.com>
2018-03-04 17:23:40 +0100

    Big rewrite

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
+//Save an option
+let saveOption = function(obj, key, value) {
+    if(typeof obj[key] === "undefined") {
+        obj[key] = value;
+    }
+    else {
+        //Check if the current option is not an array
+        if(Array.isArray(obj[key]) === false) {
+            obj[key] = [obj[key]];
+        }
+        obj[key].push(value);
+    }
+};
+
 //Get the CLI arguments
-module.exports = function(args)
-{
-  //Get the node args
-  if(typeof args === 'undefined'){ var args = process.argv.slice(2); } 
-
-  //Output arguments
-  var out = { command: '', arguments: [], options: {} };
-
-  //Read all the arguments
-  for(var i = 0; i < args.length; i++)
-  {
-    //Get the argument
-    var a = args[i];
-
-    //Check if argument starts with -
-    if(a.substring(0, 1) !== '-')
-    {
-      //Check for the command
-      if(out.command === '' && a.indexOf('/') < 0)
-      {
-        //Save as a command
-        out.command = a;
-      }
-      else if(out.command !== '')
-      {
-        //Set as a argument
-        out.arguments.push(a);
-      }
-
-      //Continue with the next argument
-      continue;
+module.exports = function (args) {
+    if (typeof args === "undefined" || Array.isArray(args) === false) {
+        //Get the arguments passed to the node CLI
+        args = process.argv.slice(2);
     }
+    let out = {arguments: [], options: {}};
+    //Parse all arguments
+    for (let i = 0; i < args.length; i++) {
+        let arg = args[i];
 
-    //Replace the -
-    a = a.replace('-', '');
+        //Check if argument does not start with a single hyphen
+        if (arg.substring(0, 1) !== "-") {
+            //Save to the non hyphenated arguments list
+            out.arguments.push(arg);
+        }
 
-    //Check for ahoter -
-    if(a.substring(0, 1) === '-'){ a = a.replace('-', ''); }
+        //Check for a double hyphenated argument
+        else if(arg.indexOf("--") === 0) {
+            let key = arg.substring(2);
+            let value = true;
+            if(i + 1 < args.length) {
+                if(args[i + 1].charAt(0) !== "-") {
+                    value = args[i + 1];
+                    i = i + 1;
+                }
+            }
+            //Save this option
+            saveOption(out.options, key, value);
+        }
 
-    //Check for invalid argument
-    if(a === '' || a === ' '){ throw new Error('Invalid argument ' + args[i]); }
-
-    //Check the next argument
-    if(i + 1 >= args.length)
-    {
-      //Set the argument as true
-      out.options[a] = true;
+        //Check for a single  hyphenated argument
+        else if(arg.indexOf("-") === 0) {
+            let keys = arg.substring(1).split("");
+            for(let j = 0; j < keys.length; j++) {
+                let key = keys[j];
+                let value = true;
+                //Check for the last character
+                if(j === keys.length - 1) {
+                    if(i + 1 < args.length) {
+                        if(args[i + 1].charAt(0) !== "-") {
+                            value = args[i + 1];
+                            i = i + 1;
+                        }
+                    }
+                }
+                //Save this option
+                saveOption(out.options, key, value);
+            }
+        }
     }
-    else if(args[i + 1].substring(0, 1) === '-')
-    {
-      //Set the argument as true
-      out.options[a] = true;
-    }
-    else
-    {
-      //Set the argument as value
-      out.options[a] = args[i + 1];
-
-      //Increment the i
-      i = i + 1;
-    }
-  }
-
-  //Return the arguments
-  return out;
+    return out;
 };
